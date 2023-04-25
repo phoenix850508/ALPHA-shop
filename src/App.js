c80a223e0b1c439e96cb2971864fa8aad060600e
phoenix850508 <tom850508@gmail.com>
2023-04-25 14:50:00 +0800

    feat: add Step1 component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import StepProgress from "./components/step/StepProgress.js";
+import Step1 from "./components/step/Step1.js";
 
 function App() {
-  return <StepProgress />;
+  return (
+    <>
+      <StepProgress />
+      <Step1 />
+    </>
+  );
 }
 
 export default App;
