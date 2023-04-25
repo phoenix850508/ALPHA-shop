2b73df48dba83ab0c08f28b4f8da7b33caa54a89
phoenix850508 <tom850508@gmail.com>
2023-04-25 14:41:52 +0800

    fix: show svg file and add bootstrap

--- src/components/step/stepProgress.js
+++ src/components/step/stepProgress.js
@@ -1,11 +1,11 @@
+import complete from "../../icons/pg-complete.svg";
+
 function Step(props) {
   return (
     <span className="progress-group" data-phase={props.phase}>
       <span className="progress-icon">
         <span className="text">{props.text}</span>
-        <svg className="icon cursor-point">
-          <use xlinkHref="#svg-icon-pg-complete" />
-        </svg>
+        <img src={complete} />
       </span>
       <span className="progress-label">{props.step}</span>
     </span>
@@ -17,7 +17,7 @@ export default function StepProgess() {
     <>
       <h2 class="register-title col col-12">結帳</h2>
       <section className="progress-container col col-12">
-        <Step data={"phase"} phase={"address"} text={1} step={"寄送地址"} />
+        <Step phase={"address"} text={1} step={"寄送地址"} />
         <Step phase={"shipping"} text={2} step={"運送方式"} />
         <Step phase={"credit-card"} text={3} step={"付款資訊"} />
       </section>
