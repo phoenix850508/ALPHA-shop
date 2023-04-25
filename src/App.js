69c7ec31961013c1e36e0526bb36cc759a819627
phoenix850508 <tom850508@gmail.com>
2023-04-25 14:58:08 +0800

    feat: add Step2 and Step3 component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import StepProgress from "./components/step/StepProgress.js";
 import Step1 from "./components/step/Step1.js";
+import Step2 from "./components/step/Step2.js";
+import Step3 from "./components/step/Step3.js";
 
 function App() {
   return (
     <>
       <StepProgress />
-      <Step1 />
+      <section className="form-container col col-12">
+        <Step1 />
+        <Step2 />
+        <Step3 />
+      </section>
     </>
   );
 }
