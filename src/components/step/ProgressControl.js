142b557414083e1d7b4fcf35a4b03b0f469682ae
phoenix850508 <tom850508@gmail.com>
2023-04-28 09:04:59 +0800

    fix: breakdown ProgressControl components

--- src/components/step/ProgressControl.js
+++ src/components/step/ProgressControl.js
@@ -2,47 +2,49 @@ import rightArrow from "../../icons/right-arrow.svg";
 import leftArrow from "../../icons/left-arrow.svg";
 import styles from "../styles/ProgressControl.module.css";
 
+export function ControlNext() {
+  return (
+    <button className={styles.btnNext}>
+      下一步
+      <img
+        className={`cursor-point ${styles.arrow}`}
+        src={rightArrow}
+        alt="left-arrow.svg"
+      />
+    </button>
+  );
+}
+
+export function ControlPrevious() {
+  return (
+    <button className={styles.btnPrevious}>
+      <img
+        className={`cursor-point ${styles.arrow}`}
+        src={leftArrow}
+        alt="left-arrow.svg"
+      />
+      上一步
+    </button>
+  );
+}
+
+export function ControlSubmit() {
+  return <button className={styles.btnNext}>確認下單</button>;
+}
+
 export default function ProgressControl() {
   return (
     <>
       <section className={`${styles.buttonGroup}`} data-phase="address">
-        <button className={styles.btnNext}>
-          下一步
-          <img
-            className={`cursor-point ${styles.arrow}`}
-            src={rightArrow}
-            alt="left-arrow.svg"
-          />
-        </button>
+        <ControlNext />
       </section>
       <section className={`${styles.buttonGroup}`} data-phase="shipping">
-        <button className={styles.btnPrevious}>
-          <img
-            className={`cursor-point ${styles.arrow}`}
-            src={leftArrow}
-            alt="left-arrow.svg"
-          />
-          上一步
-        </button>
-        <button className={styles.btnNext}>
-          下一步
-          <img
-            className={`cursor-point ${styles.arrow}`}
-            src={rightArrow}
-            alt="right-arrow.svg"
-          />
-        </button>
+        <ControlPrevious />
+        <ControlNext />
       </section>
       <section className={`${styles.buttonGroup}`} data-phase="credit-card">
-        <button className={styles.btnPrevious}>
-          <img
-            className={`cursor-point ${styles.arrow}`}
-            src={leftArrow}
-            alt="right-arrow.svg"
-          />
-          上一步
-        </button>
-        <button className={styles.btnNext}>確認下單</button>
+        <ControlPrevious />
+        <ControlSubmit />
       </section>
     </>
   );
